84e9f4435081e7ec9d0b45c5c4072add6e5feaf3
Thales Alves <thalesalves758@gmail.com>
2022-09-09 16:33:26 -0300

    feat: create credential response data type

--- src/types/credential.types.ts
+++ src/types/credential.types.ts
@@ -3,3 +3,5 @@ import { Credential } from '@prisma/client';
 export type CredentialInsertData = Omit<Credential, 'id'>;
 
 export type CredentialBodyData = Omit<CredentialInsertData, 'userId'>;
+
+export type CredentialResponseData = Omit<Credential, 'userId'>;

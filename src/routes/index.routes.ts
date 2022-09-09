5019630e508683fbddeaeef7de95c9094c5b8599
Thales Alves <thalesalves758@gmail.com>
2022-09-09 14:40:55 -0300

    feat: import and use credentials router

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRouter from './auth.routes';
+import credentialsRouter from './credentials.routes';
 
 const router = Router();
 
 router.use(authRouter);
+router.use(credentialsRouter);
 
 export default router;

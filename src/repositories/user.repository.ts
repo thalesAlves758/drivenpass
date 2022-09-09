ac2133c564993508e0c1deac490ec1bb5a36a952
Thales Alves <thalesalves758@gmail.com>
2022-09-08 23:55:23 -0300

    feat: add create new user function

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,11 @@
 import { User } from '@prisma/client';
 import { prisma } from '../config/database';
+import { AuthData } from '../types/user.types';
 
 export async function findByEmail(email: string): Promise<User | null> {
   return prisma.user.findUnique({ where: { email } });
+}
+
+export async function create({ email, password }: AuthData): Promise<void> {
+  await prisma.user.create({ data: { email, password } });
 }

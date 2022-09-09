bb44f05d5b3cb89a7e56383802bdfd25354826e3
Thales Alves <thalesalves758@gmail.com>
2022-09-09 14:47:01 -0300

    feat: add create function

--- src/repositories/credential.repository.ts
+++ src/repositories/credential.repository.ts
@@ -1,5 +1,6 @@
 import { Credential } from '@prisma/client';
 import { prisma } from '../config/database';
+import { CredentialInsertData } from '../types/credential.types';
 
 export async function findByTagAndUserId(
   userId: number,
@@ -8,4 +9,8 @@ export async function findByTagAndUserId(
   return prisma.credential.findUnique({
     where: { id_tag: { id: userId, tag } },
   });
+}
+
+export async function create(data: CredentialInsertData): Promise<void> {
+  await prisma.credential.create({ data });
 }

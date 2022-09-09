c94d4ff33c6d69e75130fe2845b6806bf9b097b2
Thales Alves <thalesalves758@gmail.com>
2022-09-09 16:33:09 -0300

    feat: add decrypt text function

--- src/utils/cryptrFunctions.ts
+++ src/utils/cryptrFunctions.ts
@@ -8,3 +8,7 @@ const cryptr = new Cryptr(process.env.CRYPTR_SECRET as string);
 export function encryptText(text: string): string {
   return cryptr.encrypt(text);
 }
+
+export function decryptText(encryptedText: string): string {
+  return cryptr.decrypt(encryptedText);
+}

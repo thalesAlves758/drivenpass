5144ee3a05a7ee6a41ecda3f6143ad5b56dbe7e4
Thales Alves <thalesalves758@gmail.com>
2022-09-09 10:35:53 -0300

    feat: create sign in function

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -3,7 +3,8 @@ import { HttpError } from '../exceptions/HttpException';
 import { create, findByEmail } from '../repositories/user.repository';
 import { HttpErrorType } from '../types/http.types';
 import { AuthData } from '../types/user.types';
-import { encryptPassword } from '../utils/bcryptFunctions';
+import { encryptPassword, validatePassword } from '../utils/bcryptFunctions';
+import { generateToken } from '../utils/jwtFunctions';
 
 export async function findUserByEmail(email: string): Promise<User | null> {
   return findByEmail(email);
@@ -19,4 +20,14 @@ export async function signUp({ email, password }: AuthData): Promise<void> {
   const encryptedPassword = encryptPassword(password);
 
   await create({ email, password: encryptedPassword });
+}
+
+export async function signIn({ email, password }: AuthData): Promise<string> {
+  const user = await findUserByEmail(email);
+
+  if (!user || !validatePassword(user.password, password)) {
+    throw HttpError(HttpErrorType.UNAUTHORIZED, `Wrong email and/or password`);
+  }
+
+  return generateToken({ userId: user.id });
 }

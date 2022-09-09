51075e2e6b58d96f6443524ffd9c65072a61963d
Thales Alves <thalesalves758@gmail.com>
2022-09-09 13:50:40 -0300

    feat: create validate token function

--- src/utils/jwtFunctions.ts
+++ src/utils/jwtFunctions.ts
@@ -1,5 +1,18 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export function generateToken(payload: string | object): string {
   return jwt.sign(payload, process.env.JWT_SECRET as string);
+}
+
+export function validateToken(token: string): JwtPayload | string | null {
+  try {
+    const decoded: string | JwtPayload = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    );
+
+    return decoded;
+  } catch (error) {
+    return null;
+  }
 }

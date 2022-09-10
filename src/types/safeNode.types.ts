42ed61014986852885b5b02a91c7036ed85e5551
Thales Alves <thalesalves758@gmail.com>
2022-09-10 12:25:46 -0300

    feat: create SafeNoteResponseData type

--- src/types/safeNode.types.ts
+++ src/types/safeNode.types.ts
@@ -3,3 +3,5 @@ import { SafeNote } from '@prisma/client';
 export type SafeNoteInsertData = Omit<SafeNote, 'id'>;
 
 export type SafeNoteBodyData = Omit<SafeNoteInsertData, 'userId'>;
+
+export type SafeNoteResponseData = Omit<SafeNote, 'userId'>;

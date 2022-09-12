4c221983bef3d114c1cfb120111a125ee1d73b67
Thales Alves <thalesalves758@gmail.com>
2022-09-12 10:35:32 -0300

    feat: create CardResponseData type

--- src/types/card.types.ts
+++ src/types/card.types.ts
@@ -3,3 +3,5 @@ import { Card } from '@prisma/client';
 export type CardInsertData = Omit<Card, 'id'>;
 
 export type CardBodyData = Omit<CardInsertData, 'userId'>;
+
+export type CardResponseData = Omit<Card, 'userId'>;

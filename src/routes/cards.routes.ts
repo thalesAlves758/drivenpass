f75eaeaba5b005c60a478a4a0cc84537b28d95a7
Thales Alves <thalesalves758@gmail.com>
2022-09-12 10:18:46 -0300

    feat: create POST /cards route

---
+++ src/routes/cards.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import validateToken from '../middlewares/validateToken.middleware';
+import * as cardController from '../controllers/card.controller';
+import validateSchema from '../middlewares/validateSchema.middleware';
+import cardSchema from '../schemas/card.schema';
+
+const cardsRouter = Router();
+
+cardsRouter.post(
+  '/cards',
+  validateToken,
+  validateSchema(cardSchema),
+  cardController.create
+);
+
+export default cardsRouter;

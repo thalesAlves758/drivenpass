f719ea1ddace140bb655ae06fce6b613c28745f5
Thales Alves <thalesalves758@gmail.com>
2022-09-12 10:34:15 -0300

    feat: create a function to get cards by user id

--- src/repositories/card.repository.ts
+++ src/repositories/card.repository.ts
@@ -12,3 +12,35 @@ export async function findByTagAndUserId(
 export async function insert(data: CardInsertData): Promise<void> {
   await prisma.card.create({ data });
 }
+
+type SelectFields = {
+  id?: boolean;
+  tag?: boolean;
+  number?: boolean;
+  cardholderName?: boolean;
+  securityCode?: boolean;
+  password?: boolean;
+  virtual?: boolean;
+  type?: boolean;
+  userId?: boolean;
+};
+
+export async function findFromUserId(
+  userId: number,
+  selectedFields: SelectFields = {
+    id: true,
+    tag: true,
+    number: true,
+    cardholderName: true,
+    securityCode: true,
+    password: true,
+    virtual: true,
+    type: true,
+    userId: true,
+  }
+): Promise<Partial<Card>[]> {
+  return prisma.card.findMany({
+    select: selectedFields,
+    where: { userId },
+  });
+}

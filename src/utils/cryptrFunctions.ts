ccf396bbf4eb8364e9f90202b4b210b92ab8393a
Thales Alves <thalesalves758@gmail.com>
2022-09-12 15:31:28 -0300

    feat: create a function to decrypt text in arrays objects

--- src/utils/cryptrFunctions.ts
+++ src/utils/cryptrFunctions.ts
@@ -12,3 +12,16 @@ export function encryptText(text: string): string {
 export function decryptText(encryptedText: string): string {
   return cryptr.decrypt(encryptedText);
 }
+
+export function decryptFromArrayObject<T, Encrypteds>(
+  array: T[],
+  encryptedFields: Partial<Encrypteds>
+): T[] {
+  return array.map((item: any) => {
+    Object.keys(encryptedFields).forEach((field) => {
+      item[field] = decryptText(item[field]);
+    });
+
+    return item;
+  });
+}

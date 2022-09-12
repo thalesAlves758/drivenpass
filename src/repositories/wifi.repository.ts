49982d80c198e71d7a6e4f1072eeefb09542ae10
Thales Alves <thalesalves758@gmail.com>
2022-09-12 14:09:51 -0300

    feat: create a function to find wifi from user

--- src/repositories/wifi.repository.ts
+++ src/repositories/wifi.repository.ts
@@ -1,6 +1,28 @@
+import { WiFi } from '@prisma/client';
 import { prisma } from '../config/database';
 import { WiFInsertData } from '../types/wifi.types';
 
 export async function insert(data: WiFInsertData): Promise<void> {
   await prisma.wiFi.create({ data });
+}
+
+type SelectFields = {
+  id?: boolean;
+  tag?: boolean;
+  password?: boolean;
+  name?: boolean;
+  userId?: boolean;
+};
+
+export async function findFromUserId(
+  userId: number,
+  selectedFields: SelectFields = {
+    id: true,
+    tag: true,
+    password: true,
+    name: true,
+    userId: true,
+  }
+): Promise<Partial<WiFi>[]> {
+  return prisma.wiFi.findMany({ select: selectedFields, where: { userId } });
 }

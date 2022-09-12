876b9a928d7e09a50cbb1432e55bec711e0cc402
Thales Alves <thalesalves758@gmail.com>
2022-09-12 13:20:59 -0300

    feat: import wifi router and use

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -3,6 +3,7 @@ import authRouter from './auth.routes';
 import cardsRouter from './cards.routes';
 import credentialsRouter from './credentials.routes';
 import safeNotesRouter from './safeNotes.routes';
+import wifiRouter from './wifi.routes';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use(authRouter);
 router.use(credentialsRouter);
 router.use(safeNotesRouter);
 router.use(cardsRouter);
+router.use(wifiRouter);
 
 export default router;

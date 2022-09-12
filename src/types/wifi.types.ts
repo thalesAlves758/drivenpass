d041e84f6b7426f0834e16a4e9c70ddc1e17d54d
Thales Alves <thalesalves758@gmail.com>
2022-09-12 14:09:31 -0300

    feat: create WiFiResponseData type

--- src/types/wifi.types.ts
+++ src/types/wifi.types.ts
@@ -3,3 +3,5 @@ import { WiFi } from '@prisma/client';
 export type WiFInsertData = Omit<WiFi, 'id'>;
 
 export type WiFiBodyData = Omit<WiFInsertData, 'userId'>;
+
+export type WiFiResponseData = Omit<WiFi, 'userId'>;

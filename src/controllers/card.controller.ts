49ae294ce93c6a3dcb27b324c80106285b074b24
Thales Alves <thalesalves758@gmail.com>
2022-09-12 10:36:12 -0300

    feat: create a handler to get all cards from user

--- src/controllers/card.controller.ts
+++ src/controllers/card.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
-import { createCard } from '../services/card.services';
-import { CardBodyData, CardInsertData } from '../types/card.types';
+import { createCard, findCardsFromUserId } from '../services/card.services';
+import {
+  CardBodyData,
+  CardInsertData,
+  CardResponseData,
+} from '../types/card.types';
 import { HttpStatus } from '../types/http.types';
 import { UserData } from '../types/user.types';
 
@@ -16,4 +20,12 @@ export async function create(req: Request, res: Response) {
   await createCard(insertData);
 
   res.sendStatus(HttpStatus.CREATED);
+}
+
+export async function getAll(req: Request, res: Response) {
+  const { id: userId }: UserData = res.locals.user;
+
+  const cards: CardResponseData[] = await findCardsFromUserId(userId);
+
+  res.send(cards);
 }
